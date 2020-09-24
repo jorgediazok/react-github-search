c709940c84e835e0171ab588d74750f7989df11c
Jorge <jorgediazok@gmail.com>
2020-09-24 00:55:30 -0300

    making the searchbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = useState('');
+  //get things from global context
+  const handleSubmit = (e) => {
+    console.log(user);
+  };
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input type="text" placeholder="enter github user" />
+            <button type="submit">Search</button>
+          </div>
+        </form>
+        <h3>requests : 60 / 60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
